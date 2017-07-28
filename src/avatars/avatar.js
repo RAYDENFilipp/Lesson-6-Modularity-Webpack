7fb902fe12eb6eb158d812f01a0653bf794e4fdd
Philipp Lypniakov <filipprayden@gmail.com>
2017-07-28 22:39:12 +0300

    Avatars appended to each name

---
+++ src/avatars/avatar.js
@@ -0,0 +1,11 @@
+module.exports = [
+  '../avatars/1',
+  '../avatars/2',
+  '../avatars/3',
+  '../avatars/4',
+  '../avatars/5',
+  '../avatars/6',
+  '../avatars/7',
+  '../avatars/8',
+  '../avatars/9',
+]
--- src/js/userList.js
+++ src/js/userList.js
@@ -1,12 +1,15 @@
 var _ = require('lodash');
+var avatars = require('../avatars/avatar');
 
 function userList(users){
     const container = document.getElementById('root');
     const sortedUsers = _.sortBy(users, 'age');
     this.showList = () => {
         sortedUsers.forEach((user) => {
+          let index = 0;
             const div = document.createElement("div");
-            div.append(user.name + ' ' + user.age);
+            div.append(`<img src=${avatars[index]} alt='avatar ${index++}'>`);
+            div.append( user.name + ' ' + user.age);
             container.appendChild(div);
         });
     }
