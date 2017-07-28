77011c82dd5895e8c53809356033b52c826886bf
Philipp Lypniakov <filipprayden@gmail.com>
2017-07-29 00:29:49 +0300

    Autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -8,30 +8,58 @@ module.exports = {
 		path: __dirname + "/dist",
 		filename: "bundle.js"
 	},
+	parser: 'sugarss',
+	plugins: {
+		'postcss-import': {
+			root: file.dirname
+		},
+		'postcss-cssnext': options.cssnext ? options.cssnext : false,
+		'autoprefixer': env == 'production' ? options.autoprefixer : false,
+		'cssnano': env === 'production' ? options.cssnano : false
+	},
 	module: {
 		rules: [{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
+				use: ['style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1
+						}
+					},
+					'postcss-loader'
+				]
 			},
 			{
 				test: /\.scss$/,
-				use: ['style-loader', 'sass-loader']
+				use: ['style-loader',
+					{
+						loader: 'sass-loader',
+						options: {
+							importLoaders: 1
+						}
+					},
+					'postcss-loader'
+				]
 			},
 			{
 				test: /\.js$/,
 				enforce: 'pre',
 				exclude: /(node_modules)/,
-				use: [
+				use: [{
+						loader: 'babel-loader',
+						options: {
+							presets: ['env']
+						}
+					},
 					{
-					loader: 'babel-loader',
-					options: {
-						presets: ['env']
+						loader: 'jshint-loader'
 					}
-				},
-				{
-					loader: 'jshint-loader'
-				}
-			]
+				]
+			},
+			{
+				test: /\.png$/,
+				use: ['file-loader']
 			}
 		]
 	},
